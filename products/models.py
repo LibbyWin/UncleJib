bf817f0c4b3a39829448c00985bdd995de483407
Wini996 <libbywinfield101@gmail.com>
2020-03-18 15:10:49 +0000

    added products.html full info and edit to the models for products to allow more info for each product

--- products/models.py
+++ products/models.py
@@ -3,12 +3,21 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     """ adding multiple images """
-    product.title = models.CharField(max_length=250, default='')
+    title = models.CharField(max_length=250, default='')
     product_image_01 = models.ImageField(upload_to='images', blank=True)
     product_image_02 = models.ImageField(upload_to='images', blank=True, null=True)
     product_image_03 = models.ImageField(upload_to='images', blank=True, null=True) 
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
+    size = models.CharField(max_length=100, default='')
+    colour = models.CharField(max_length=100, default='')
+    gender = models.CharField(max_length=100, default='')
+    year = models.CharField(max_length=100, default='')
+    flex = models.CharField(max_length=100, default='')
+    shape = models.CharField(max_length=100, default='')
+    rider_prodile = models.CharField(max_length=100, default='')
+    board_proflie = models.CharField(max_length=100, default='')
+    condition = models.CharField(max_length=100, default='')
    
     def __str__(self):
         return self.name
