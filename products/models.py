147e494bd34ff37dee89e40e807ef617e1be9c52
Wini996 <libbywinfield101@gmail.com>
2020-03-19 15:32:55 +0000

    migration to the model.py products

--- products/models.py
+++ products/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Product(models.Model):
     """ adding multiple images """
     title = models.CharField(max_length=250, default='')
-    product_image_01 = models.ImageField(upload_to='images', blank=True)
-    product_image_02 = models.ImageField(upload_to='images', blank=True, null=True)
-    product_image_03 = models.ImageField(upload_to='images', blank=True, null=True) 
+    image_01 = models.ImageField(upload_to='images', blank=True)
+    image_02 = models.ImageField(upload_to='images', blank=True, null=True)
+    image_03 = models.ImageField(upload_to='images', blank=True, null=True) 
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
     size = models.CharField(max_length=100, default='')
