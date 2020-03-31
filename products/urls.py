ab528b0b66c495cd9dcdc23c6cccffa609ba0e1e
Wini996 <libbywinfield101@gmail.com>
2020-03-31 21:36:51 +0000

    edit to the products app including the views.py file adding new content to the urls and index page to access fully working links

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
-from .views import all_products, all_categories
+from .views import all_categories, product, all_products
 
 urlpatterns = [
-    url(r'^$', all_products, name='product'),
-    url(r'categories/$', all_categories, name='categories'),
+    url(r'^$', all_products, name='products'),
+    url(r'all_mountain$', product, name='all_mountain'),
+    url(r'big_mountain$', product, name='big_mountain'),
+    url(r'freestyle$', product, name='freestyle'),
+    url(r'pipe/park$', product, name='pipe/park'),
+    url(r'jib/street$', product, name='jib/street'),
+    url(r'boardercross$', product, name='boardercross'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,10 +2,26 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
-def all_products(request):
-    products = Product.objects.all
-    return render(request, "products.html", {"products": products})
-
 def all_categories(request):
+    """
+    shows all products under a specific category 
+    ie. freestyle = feestyle snowboards
+    """
     product_list = Product.objects.filter(choice=current_url).order_by('name')
     return render(request, "categories.html", {'products': products})
+
+def product(request):
+    """
+    show an individual product with all its detail
+    show on the page
+    """
+    product = Review.objects.filter(product=product)    
+    return render(request, "product.html", {"product": product})
+
+def all_products(request):
+    """
+    Show all products when /products is added onto the 
+    sites url
+    """
+    products = Product.objects.all()
+    return render(request, "categories.html", {"products": products})
