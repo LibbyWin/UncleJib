9d74888c9d469ed78b706a1910cb6eb6733e8993
Wini996 <libbywinfield101@gmail.com>
2020-03-31 15:48:16 +0000

    added function to all pages

--- products/models.py
+++ products/models.py
@@ -16,30 +16,20 @@ class Category(models.Model):
         return self.snowboard_category
 
 class Product(models.Model):
-    """ adding multiple images """
-    title = models.CharField(max_length=250, default='')
+    name = models.CharField(max_length=250, default='')
     slug = models.SlugField(max_length=200, unique=True)
-    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
     image_01 = models.ImageField(upload_to='images', blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField(blank=True)
     size = models.CharField(max_length=100, default='')
     colour = models.CharField(max_length=100, default='')
-    gender = models.CharField(max_length=100, default='')
-    year = models.CharField(max_length=100, default='')
     flex = models.CharField(max_length=100, default='')
     shape = models.CharField(max_length=100, default='')
-    rider_profile = models.CharField(max_length=100, default='')
     board_profile = models.CharField(max_length=100, default='')
-    condition = models.CharField(max_length=100, default='')
     stock = models.IntegerField(blank=True)
     available = models.BooleanField(default=True)
  
-    class Meta:
-        ordering = ('title',)
-        verbose_name = 'product'
-        verbose_name_plural = 'products'
    
     def __str__(self):
-        return self.title
+        return self.name
 
