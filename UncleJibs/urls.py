1e380095369d074459de64e287f7bf8437fdbed7
Wini996 <libbywinfield101@gmail.com>
2020-04-04 17:07:28 +0000

    added correction to the carcheckout html making all the links work

--- UncleJibs/urls.py
+++ UncleJibs/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
 from products import urls as urls_products
+from checkout import urls as urls_checkout
 from products.views import all_products, category, product
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -30,6 +31,7 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^pages/', include(urls_pages)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
