c00ef9de939ed79d3a3dd32e53205bfbcdf2e674
Wini996 <libbywinfield101@gmail.com>
2020-04-14 12:50:01 +0000

    added unckejibs to static AWS and all works

--- UncleJibs/settings.py
+++ UncleJibs/settings.py
@@ -141,13 +141,13 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=94608000'
 }
 
-AWS_STORAGE_BUCKET_NAME = 'uncle-jibs'
+AWS_STORAGE_BUCKET_NAME = 'unclejibs'
 AWS_S3_REGION_NAME = 'eu-west-2'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-#STATIC_FILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
